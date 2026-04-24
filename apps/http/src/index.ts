4fddb5be0706f29124f18e89ac9aca3a0da2e14c
navu545 <navu545@gmail.com>
2026-04-24 06:20:10 +0530

    fix: dynamic port for deployment

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -24,6 +24,8 @@ app.use("/users", userRoutes);
 app.use(errorHandler);
 connectWS();
 
-app.listen(3001, () => {
-  console.log("HTTP server running on port 3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`HTTP server running on port ${PORT}`);
 });
